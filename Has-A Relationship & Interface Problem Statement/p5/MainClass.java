bdaa9631e8c3f20a9b0a0d6a160891f24a99246e
hairilhumsani <hairilhumsani@gmail.com>
2022-06-08 14:41:53 +0800

    commit

--- Has-A Relationship & Interface Problem Statement/p5/MainClass.java
+++ Has-A Relationship & Interface Problem Statement/p5/MainClass.java
@@ -4,6 +4,32 @@ public class MainClass implements EmployeeService{
 	
 	public static void main(String[] args) {
 		
+		
+		
+	}
+
+	@Override
+	public Employee[] getAllEmployees(Project project) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getSalary(int empId, Project project) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getTotalSalaryPaid(Project project) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String[] getEmployeeNames(Project project) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	
