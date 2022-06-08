fcba5e54c7e77cc1dcbf31ffeb8cc4fbf84c68d7
hairilhumsani <hairilhumsani@gmail.com>
2022-06-08 12:34:56 +0800

    changes

--- Has-A Relationship & Interface Problem Statement/p5/MainClass.java
+++ Has-A Relationship & Interface Problem Statement/p5/MainClass.java
@@ -1,9 +1,33 @@
 package p5;
 
-public abstract class MainClass implements EmployeeService{
+public class MainClass implements EmployeeService{
 	
 	public static void main(String[] args) {
 		
+	}
+
+	@Override
+	public Employee[] getAllEmployees(String projectName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getSalary(int empId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getTotalSalaryPaid(String projectName) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String[] getEmployeeNames(String projectName) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	
