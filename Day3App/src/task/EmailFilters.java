2ee844a1106e7364dc27b6d9396106855684fe3d
hairilhumsani <hairilhumsani@gmail.com>
2022-06-08 11:07:52 +0800

    emailFilters

---
+++ Day3App/src/task/EmailFilters.java
@@ -0,0 +1,33 @@
+package task;
+
+public class EmailFilters {
+
+	static String emails[] = {
+
+			"ramesh@gmail.com", "rakesh@outlook.com", "lokesh@linkedin.com", "mahesh@gmail.com", "ganesh@gmail.com",
+			"rajesh@outlook.com" };
+
+	public static void main(String[] args) {
+		int gmail = 0;
+		int outlook = 0;
+		int linkedin = 0;
+
+		for (String email : emails) {
+			email.split("@");
+
+			if (email.endsWith("gmail.com")) {
+				gmail++;
+
+			}
+
+			else if (email.endsWith("outlook.com")) {
+				outlook++;
+
+			} else if (email.endsWith("linkedin.com")) {
+				linkedin++;
+
+			}
+		}
+		System.out.println("Gmail= " + gmail + " Outlook= " + outlook + " Linkin= " + linkedin);
+	};
+}
