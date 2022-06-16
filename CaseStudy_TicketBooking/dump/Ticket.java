c6911b0bab4ffb2d4c61bc2c45449a3197110320
hairilhumsani <hairilhumsani@gmail.com>
2022-06-16 17:05:19 +0800

    commit

--- CaseStudy_TicketBooking/dump/Ticket.java
+++ CaseStudy_TicketBooking/dump/Ticket.java
@@ -7,5 +7,6 @@ public class Ticket {
 	private int bookingId;
 	private int seatBooked; // number of seats to be booked
 	Map<Integer,String> auidanceName; // <seatNumber,AudianceName>
+	LocalDate ticketDate;
 	
 }
