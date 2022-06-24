75ebad8a003a8656249c6d64262d8d6a4e09b150
hairilhumsani <hairilhumsani@gmail.com>
2022-06-24 11:42:29 +0800

    update

--- WebApp/src/main/java/com/ncs/servlet/AppServlet.java
+++ WebApp/src/main/java/com/ncs/servlet/AppServlet.java
@@ -19,46 +19,63 @@ import com.ncs.service.CourseServiceImpl;
  * Servlet implementation class AppServlet
  */
 public class AppServlet extends HttpServlet {
-	
+
 	CourseService courseService;
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AppServlet() {
-    	courseService = new CourseServiceImpl();
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public AppServlet() {
+		courseService = new CourseServiceImpl();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
 		response.setContentType("text/html");
 		PrintWriter browser = response.getWriter();
-		
+
 		try {
-			List<Course> courseList = courseService.getAllCourse();
 			
-			for (Course course : courseList)
-			{
-				browser.print("<br>"+course+"</br>");
+			browser.print(
+					"<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css\" rel=\"stylesheet\">\n"
+							+ "<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js\"></script>");
+			List<Course> courseList = courseService.getAllCourse();
+
+			browser.print("<table class=\"table\">\n" + "<thead class=\"thead thead-dark\">\n" + "  <tr>\n"
+					+ "    <th scope=\"col\">#</th>\n" + "<th scope=\"col\">Course Name</th>\n"
+					+ "    <th scope=\"col\">Exams</th>\n" + "    <th scope=\"col\">Duration</th>\n"
+					+ "    <th scope=\"col\"></th>\n" + "  </tr>\n" + "</thead>");
+			browser.print("<tbody>");
+			for (Course course : courseList) {
+				browser.print("<tr>\n" + "    <th scope=\"row\">" + course.getCourseId() + "</th>\n" + "    <td>"
+						+ course.getCourseName() + "</td>\n" + "    <td>" + course.getExams() + "</td>\n" + "    <td>"
+						+ course.getDuration() + "</td>\n" + "    <td>"
+						+ "<button type=\"button\" class=\"btn btn-outline-primary\">Delete</button>"
+						+ "</td>  </tr>");
 			}
+			browser.print("</tbody>\n" + "</table>");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
-}
+};
