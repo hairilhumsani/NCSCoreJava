01989c8053d9f1232af910ccf66a86730a0c5dcb
hairilhumsani <hairilhumsani@gmail.com>
2022-07-04 11:26:54 +0800

    commit

---
+++ SpringCoreApp1/src/main/java/p1/MainClass.java
@@ -0,0 +1,29 @@
+package p1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainClass {
+	
+
+public static void main(String[] args) {
+		
+		
+		
+		ApplicationContext spring = new ClassPathXmlApplicationContext("ncs-spring-cofg.xml");
+		
+		Employee e = (Employee)spring.getBean("employee");
+		
+		printEmployeeDetails(e);
+		
+		
+		
+	}
+	
+	
+	public static void printEmployeeDetails(Employee e)
+	{
+		System.out.println(e);
+	}
+
+}
